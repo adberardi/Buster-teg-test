ee617163d0dd4a7339d04ce4c6e78420fb5335be
Antonio Berardi <adberardi@yahoo.com>
2022-06-12 17:46:39 -0400

    Segundo Commit, subiendo proyecto

---
+++ ARP Test Version/Assets/Scripts/RecoScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class RecoScript : MonoBehaviour, ITrackableEventHandler
+{
+    private TrackableBehaviour mTrackableBehaviour;
+
+    void Start()
+    {
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (mTrackableBehaviour)
+        {
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+        }
+    }
+
+    public void OnTrackableStateChanged(
+                                    TrackableBehaviour.Status previousStatus,
+                                    TrackableBehaviour.Status newStatus)
+    {
+        if (newStatus == TrackableBehaviour.Status.DETECTED ||
+            newStatus == TrackableBehaviour.Status.TRACKED ||
+            newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+        {
+            //muovo la barca
+            // Debug.Log("muovo");
+            // Spawn.current.start = true;
+            //float r = Random.Range(0, 1f);
+            Spawn.current.StartGenerator(0);
+        }
+        else
+        {
+            // rimetto la barca al suo posto
+            Spawn.current.start = false;
+            //Spawn.current.DesactivatedCharacter();
+            //  Debug.Log("fermo");
+        }
+    }
+}
---
+++ ARP Test Version/Assets/Scripts/Spawn.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class Spawn : MonoBehaviour
+{
+    public Transform prefab;
+    public Transform newParent;
+    Transform newCharacter;
+    public bool start;
+    public static Spawn current;
+
+    private void Awake()
+    {
+        current = this;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(start == true)
+        {
+            //Instantiate(prefab, transform.position, transform.rotation);
+            newCharacter.Translate(new Vector3(0, 0, 0.5f) * Time.deltaTime);
+        }
+
+    }
+
+    public void StartGenerator(float np)
+    {
+        newCharacter = Instantiate(prefab, transform.position, transform.rotation);
+        newCharacter.SetParent(newParent);
+        newCharacter.localPosition = new Vector3(0.379f+np, 0, np);
+        newCharacter.GetComponent<Animation>().Play();
+        start = true;
+        //gameObject.GetComponent<Rigidbody>().useGravity = true;
+        //Instantiate(prefab, new Vector3(0.4f, 0, 0), transform.rotation);
+    }
+
+    public void DesactivatedCharacter()
+    {
+        Destroy(newCharacter);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Wall")
+        {
+            //Destroy(newCharacter);
+            newCharacter.GetComponent<Animation>().Stop();
+        }
+    }
+}
