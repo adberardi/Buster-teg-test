185418b8595f255665fbf3e763374823832f7b3f
Antonio Berardi <adberardi@yahoo.com>
2022-09-25 21:35:19 -0400

    Implementando isla que reacciona al touch

---
+++ ARP Test Version/Assets/Scripts/Multiplicacion/ControllerMultiplicacion.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ControllerMultiplicacion : MonoBehaviour
+{
+    public static ControllerMultiplicacion current;
+    public GameObject boat;
+    public UnityEvent OnClick = new UnityEvent();
+
+
+    private void Awake()
+    {
+        current = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ ARP Test Version/Assets/Scripts/Multiplicacion/IslandTop.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class IslandTop : MonoBehaviour
+{
+    public static IslandTop current;
+    public GameObject boat;
+    public UnityEvent OnClick = new UnityEvent();
+
+    private void Awake()
+    {
+        current = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        boat.GetComponent<Animator>().speed = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit Hit;
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Physics.Raycast(ray, out Hit) && Hit.collider.gameObject == gameObject)
+            {
+                Debug.Log("Presionando sobre isla");
+                boat.GetComponent<Animator>().speed = 1;
+                boat.GetComponent<Animator>().Play("Base Layer.MoveBoatTop");
+            }
+
+        }
+    }
+}
