8c8cb6a04df34e7e2fe6bda45877a0fe9baf75db
Antonio Berardi <adberardi@yahoo.com>
2022-10-14 23:28:43 -0400

    Implementando botones/contador de inicio de juego/Muteo del sonido

---
+++ ARP Test Version/Assets/Scripts/Multiplicacion/MultiplicationController.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class MultiplicationController : MonoBehaviour
+{
+    public static MultiplicationController current;
+    AudioSource soundGame;
+    public bool onGoingGame { get; set; }
+    int responseUser;
+    int finalResult;
+    int counterToStart;
+    public Text btnTextSound;
+    public Text textField;
+    public GameObject boat;
+    public GameObject effectsToWinner;
+    public AudioClip soundWinner;
+    public AudioClip soundLoser;
+    public UnityEvent OnClick = new UnityEvent();
+
+
+    private void Awake()
+    {
+        current = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        counterToStart = 3;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // Returns the TextField from the Game's UI
+    public Text GetTextField()
+    {
+        return textField;
+    }
+
+    // Shows the countdown to start the game
+    public void ShowCounterToStartGame()
+    {
+        if (counterToStart >= 0)
+        {
+            Invoke("UpdateCounterToStart", 1.0f);
+        }
+        else
+        {
+            onGoingGame = true;
+            soundGame.Play();
+        }
+    }
+
+    // Updates the TextField that show the countdown to start the game (void ShowCounterToStartGame)
+    private void UpdateCounterToStart()
+    {
+        MultiplicationTextScript.current.SetText(counterToStart);
+        counterToStart--;
+        Invoke("ShowCounterToStartGame", 1.0f);
+    }
+
+
+    public Text GetSoundButton()
+    {
+        return btnTextSound;
+    }
+
+    // Change the sound in the game.
+    public void UpdateSound(AudioClip newSound)
+    {
+        soundGame.Stop();
+        soundGame.clip = newSound;
+        soundGame.Play();
+        soundGame.loop = true;
+    }
+
+    // Return the audio clip it will be played
+    public AudioSource GetAudioClip()
+    {
+        return soundGame;
+    }
+
+    // Enable text indicating the correct answer
+    public void CallFinishText()
+    {
+        MultiplicationTextScript.current.FinishText(ObtainResult(), responseUser);
+        if (ObtainResult() == responseUser)
+        {
+            // User Win
+            effectsToWinner.SetActive(true);
+            UpdateSound(soundWinner);
+        }
+        else
+        {
+            // User Lose
+            UpdateSound(soundLoser);
+        }
+    }
+
+    // Return the result of people it will be inside the house.
+    public int ObtainResult()
+    {
+        return finalResult;
+    }
+}
---
+++ ARP Test Version/Assets/Scripts/Multiplicacion/SoundMultiplication.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundMultiplication : MonoBehaviour
+{
+    AudioSource soundBtn;
+    Text buttonTextSound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        buttonTextSound = MultiplicationController.current.GetSoundButton();
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // Mutes the sound in the game and change the button's text
+    public void MuteSound()
+    {
+        soundBtn = MultiplicationController.current.GetAudioClip();
+        soundBtn.mute = !soundBtn.mute;
+        if (soundBtn.mute)
+        {
+            buttonTextSound.text = "Activar Música";
+        }
+        else
+        {
+            buttonTextSound.text = "Desactivar Música";
+        }
+    }
+}
