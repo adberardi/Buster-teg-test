4c628aeffdf06fbc802811ec09c0d9d3e7f5ce71
Antonio Berardi <adberardi@yahoo.com>
2023-01-01 23:30:44 -0400

    Subiendo archivos Menu Scene y Action.cs

---
+++ ARP Test Version/Assets/Scripts/Actions.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Actions : MonoBehaviour
+{
+    Firebase.Auth.FirebaseAuth auth;
+    public InputField email;
+    public InputField passw;
+    // Start is called before the first frame update
+    void Start()
+    {
+        auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CreateUser()
+    {
+
+        auth.CreateUserWithEmailAndPasswordAsync(email.text, passw.text).ContinueWith(task => {
+            if (task.IsCanceled)
+            {
+                Debug.LogError("CreateUserWithEmailAndPasswordAsync was canceled.");
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                Debug.LogError("CreateUserWithEmailAndPasswordAsync encountered an error: " + task.Exception);
+                return;
+            }
+
+            // Firebase user has been created.
+            Firebase.Auth.FirebaseUser newUser = task.Result;
+            Debug.LogFormat("Firebase user created successfully: {0} ({1})",
+                newUser.DisplayName, newUser.UserId);
+        });
+    }
+
+    public void Login()
+    {
+        auth.SignInWithEmailAndPasswordAsync(email.text, passw.text).ContinueWith(task => {
+            if (task.IsCanceled)
+            {
+                Debug.LogError("SignInWithEmailAndPasswordAsync was canceled.");
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                Debug.LogError("SignInWithEmailAndPasswordAsync encountered an error: " + task.Exception);
+                return;
+            }
+
+            Firebase.Auth.FirebaseUser newUser = task.Result;
+            Debug.LogFormat("User signed in successfully: {0} ({1})",
+                newUser.DisplayName, newUser.UserId);
+        });
+    }
+}
