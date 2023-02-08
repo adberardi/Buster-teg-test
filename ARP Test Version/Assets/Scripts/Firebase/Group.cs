6dd2a6e109bedf40b54f39a75f34a5c80de7f84e
Antonio Berardi <adberardi@yahoo.com>
2023-02-08 00:11:43 -0400

    Lectura de datos en Collection Group

--- ARP Test Version/Assets/Scripts/Firebase/Group.cs
+++ ARP Test Version/Assets/Scripts/Firebase/Group.cs
@@ -11,11 +11,11 @@ namespace ARProject.Group
     {
         private int IdGroup { get; set; }
         private string NombreGroup { get; set; }
-        private DateTime FechaCreacion { get; set; }
+        private Timestamp FechaCreacion { get; set; }
         private string Admin { get; set; }
         // TODO: Por agregar clases Estudiante y Tarea
-        private ArrayList Participante { get; set; }
-        private ArrayList Asignacion { get; set; }
+        private List<object> Participante { get; set; }
+        private List<object> Asignacion { get; set; }
 
         private FirebaseFirestore db;
         private FirestoreError ErrorCode;
@@ -33,7 +33,9 @@ namespace ARProject.Group
             {
                 { "admin", "8nkf5pBPwFcRhUjShQmnwmlPlyE3" },
                 { "name", "Grupo 1er grado teresiano" },
-                { "participantsGroup", new ArrayList() {"b72406d79978ea67095af8c75c24eac0","8nkf5pBPwFcRhUjShQmnwmlPlyE3", "8f128e1e2473092b221b2a89030b817e" } },
+                { "assignedActivities", new List<object>() { "Task/PrimeraTarea" } },
+                { "dateCreated", DateTime.Now },
+                { "participantsGroup", new List<object>() {"b72406d79978ea67095af8c75c24eac0","8nkf5pBPwFcRhUjShQmnwmlPlyE3", "8f128e1e2473092b221b2a89030b817e" } },
 
             };
 
@@ -56,24 +58,75 @@ namespace ARProject.Group
                     {
                         DocumentSnapshot doc = task.Result;
                         Dictionary<string, object> docGroup = doc.ToDictionary();
-                        Debug.Log(string.Format("> Leyendo Grupo: Admin {0} | Name {1} | Participantes {2}", docGroup["admin"], docGroup["name"], docGroup["participantsGroup"].GetType()));
+                        //Debug.Log(string.Format("> Leyendo Grupo: Admin {0} | Name {1} | Participantes {2}", docGroup["admin"], docGroup["name"], docGroup["participantsGroup"].GetType()));
 
-                        foreach (var index in (List<object>)docGroup["participantsGroup"])
-                        {
-                            Debug.Log(string.Format("Ciclo: {0}", index));
-                            //TODO: corregir Participante.Add(index)
-                            Participante.Add(index);
-                        }
+                        Debug.Log(string.Format("> Leyendo Fecha Creacion: {0}", docGroup["dateCreated"].GetType()));
+
                         NombreGroup = docGroup["name"].ToString();
                         Admin = docGroup["admin"].ToString();
-                        //FechaCreacion = (DateTime)docGroup["dateCreated"];
-                        //Asignacion.Add((ArrayList)docGroup["assignedActivities"]);
-                        //Debug.Log(string.Format("=> Longitud Asignacion: ", Asignacion.Count));
+                        FechaCreacion = (Timestamp) docGroup["dateCreated"];
+                        Asignacion = (List<object>)docGroup["assignedActivities"];
+                        Participante = (List<object>)docGroup["participantsGroup"];
+                        Debug.Log(string.Format("=> Longitud Asignacion: {0}", Participante.Count));
+                        Debug.Log(string.Format("> Leyendo Grupo: Admin {0} | Name {1} | Participantes {2} | FechaCreacion {3} | Asignacion {4}", Admin, NombreGroup, Participante.Count, FechaCreacion  , Asignacion.Count));
                     }
 
                 });
             }
+
+        public void UpdateNameGroup ()
+        {
+            DocumentReference docRef = db.Collection("Groups").Document(new User.User().GetSessionDataUser());
+            Dictionary<string, object> groupUpdate = new Dictionary<string, object>
+            {
+                { "name", "Nuevo Nombre Grupo"}
+            };
+
+            docRef.UpdateAsync(groupUpdate).ContinueWithOnMainThread(task =>
+            {
+                if (task.IsCompleted)
+                    Debug.Log("Atualizacion de nombre para grupo completada");
+                else if (task.IsCanceled)
+                {
+                    Debug.Log("La atualizacion fue cancelada");
+                }
+                else
+                {
+                    Debug.Log("La atualizacion fue interrumpida");
+                }
+            });
         }
+
+        public void UpdateParticipantsGroup(List<object> member)
+        {
+            DocumentReference docRef = db.Collection("Groups").Document(new User.User().GetSessionDataUser());
+            Dictionary<string, object> groupUpdate = new Dictionary<string, object>
+            {
+                { "participantsGroup", member}
+            };
+
+            docRef.UpdateAsync(groupUpdate).ContinueWithOnMainThread(task =>
+            {
+                if (task.IsCompleted)
+                    Debug.Log("Atualizacion de nombre para grupo completada");
+                else if (task.IsCanceled)
+                {
+                    Debug.Log("La atualizacion fue cancelada");
+                }
+                else
+                {
+                    Debug.Log("La atualizacion fue interrumpida");
+                }
+            });
+        }
+
+        public void SendEmailNotification(ArrayList member)
+        {
+
+        }
+
+
+    }
 
     }
 
