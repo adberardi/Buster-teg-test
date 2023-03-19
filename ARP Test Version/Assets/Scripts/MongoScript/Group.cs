9287a23589fa371ae05876ed4d5aa20ad9aa44b6
Antonio Berardi <adberardi@yahoo.com>
2023-03-18 23:49:32 -0400

    Metodo Group - Agregando el crear grupo

--- ARP Test Version/Assets/Scripts/MongoScript/Group.cs
+++ ARP Test Version/Assets/Scripts/MongoScript/Group.cs
@@ -35,26 +35,11 @@ namespace ARProject.Group
         }
 
 
-        /*public void SaveGroup(string nameGroup)
+        public void CreateGroup(Group newGroup)
         {
-            DocumentReference docRef = db.Collection("Groups").Document(nameGroup);
-            Dictionary<string, object> group = new Dictionary<string, object>
-            {
-                { "admin", "8nkf5pBPwFcRhUjShQmnwmlPlyE3" },
-                { "name", "Grupo 1er grado teresiano" },
-                { "assignedActivities", new List<object>() { "Task/PrimeraTarea" } },
-                { "dateCreated",  DateTime.Now.ToString()},
-                { "participantsGroup", new List<object>() {"b72406d79978ea67095af8c75c24eac0","8nkf5pBPwFcRhUjShQmnwmlPlyE3", "8f128e1e2473092b221b2a89030b817e" } },
-
-            };
-
-            docRef.SetAsync(group).ContinueWithOnMainThread(task =>
-            {
-                Debug.Log("Se registro de manera exitosa el grupo");
-            });
-
-            CreateGroupGamePlayed(nameGroup);
-        }*/
+            Group registerGroup = new Group();
+            registerGroup.GetCollection().InsertOne(newGroup);
+        }
 
         public void ReadGroup(string IdGroup)
         {
