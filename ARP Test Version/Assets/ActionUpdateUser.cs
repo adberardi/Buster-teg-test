cad1d026b4ca7099b42ac8e728897485f4cb6e84
Antonio Berardi <adberardi@yahoo.com>
2023-04-02 21:04:19 -0400

    Metodo para actualizar datos de usuario con validaciones, listo

---
+++ ARP Test Version/Assets/ActionUpdateUser.cs
@@ -0,0 +1,48 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+using ARProject.User;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ActionUpdateUser : MonoBehaviour
+{
+    public InputField email;
+    public InputField passw;
+    public InputField userName;
+    public InputField firstName;
+    public InputField lastName;
+    //public Transform birthDay;
+    public string profile;
+    private User user;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        user = new User();
+        userName.text = user.GetDataUser()["Username"];
+        firstName.text = user.GetDataUser()["Firstname"];
+        lastName.text = user.GetDataUser()["Lastname"];
+        email.text = user.GetDataUser()["Email"];
+        Debug.Log("Entrando a Start " + user.GetDataUser()["Username"]);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UpdateUser()
+    {
+        //Debug.Log("Fecha insertada: "+ birthDay.name);
+        user.SaveUser(firstName.text,lastName.text,passw.text,email.text, "12/03/2022", profile);
+    }
+
+    public void ChangeScene(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+}
