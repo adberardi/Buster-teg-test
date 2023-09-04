74a508b6f47400c9edc25eda79c0d98532c7b496
Antonio Berardi <adberardi@yahoo.com>
2023-09-03 21:49:07 -0400

    Agrengado scripts para funcionaloidad de Escena Tutorial

---
+++ ARP Test Version/Assets/Scripts/Suma/Tutorial/Tutorial.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MongoDB.Driver;
+using MongoDB.Bson;
+
+
+namespace ARProject.Tutorial
+{
+    class Tutorial
+    {
+        public ObjectId _id { get; set; }
+        public string Description { get; set; }
+        public ObjectId IdGame { get; set; }
+        public string TitleGame { get; set; }
+        private MongoClient _client;
+
+        public Tutorial()
+        {
+            _client = MongoDBManager.GetClient();
+        }
+
+        //Establece la conexion con la base de datos
+        public IMongoCollection<Tutorial> GetCollection()
+        {
+            var db = _client.GetDatabase("Mercurio");
+            return db.GetCollection<Tutorial>("Tutorial");
+        }
+
+        //Busca el tutorial y retorna la informacion a mostrar.
+        public Tutorial GetGameTutorial()
+        {
+            IMongoCollection<Tutorial> docRef = GetCollection();
+            Tutorial data = docRef.Find(task => task._id == ObjectId.Parse(PlayerPrefs.GetString("IdGame"))).ToList()[0];
+            return data;
+        }
+
+    }
+}
---
+++ ARP Test Version/Assets/Scripts/Suma/Tutorial/TutorialController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using ARProject.Tutorial;
+
+public class TutorialController : MonoBehaviour
+{
+    Tutorial demo = new Tutorial();
+    public Text TextArea;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TextArea.text = demo.Description;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Carga el juego seleccionado por el usuario.
+    public void BtnContinue()
+    {
+        SceneManager.LoadScene(PlayerPrefs.GetInt("IdNextScene"));
+    }
+
+    //Carga la escena anterior (Pueden ser escenas como; Menu de juego de Suma, Menu de Resta o Home)
+    public void BtnBack()
+    {
+        SceneManager.LoadScene(PlayerPrefs.GetInt("IdPrevious"));
+    }
+}
