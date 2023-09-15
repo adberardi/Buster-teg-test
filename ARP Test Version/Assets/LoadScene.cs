79bb597c7c1beddfbab3811f39f05235a3056eb7
Antonio Berardi <adberardi@yahoo.com>
2023-09-14 23:32:41 -0400

    Escena tutorial: validando y removiendo palabras acentuadas y llenando informacion en la colleccion Tutorial

--- ARP Test Version/Assets/LoadScene.cs
+++ ARP Test Version/Assets/LoadScene.cs
@@ -1,9 +1,9 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using System.Text.RegularExpressions;
+using System.Text;
 
 public class LoadScene : MonoBehaviour
 {
@@ -21,11 +21,21 @@ public class LoadScene : MonoBehaviour
         
     }
 
+    //Valida si la palabra contiene acentos, en caso de que una letra lo tenga la remueve y la sustituye por la letra sin acento.
+    public string RemoveAccents(string textToPrepare)
+    {
+        string wordsWithAccents = "áéíóúñ";
+        string textWithoutAccent = Regex.Replace(wordsWithAccents.Normalize(NormalizationForm.FormD), @"[^a-zA-z0-9 ]+", "");
+        return textWithoutAccent;
+    }
+
     public void ChangeScene(int index)
     {
         PlayerPrefs.SetInt("IdPreviousScene", SceneManager.GetActiveScene().buildIndex);
         SceneManager.LoadScene(index);
         PlayerPrefs.SetInt("IdNextScene", IdNextScene);
+
+        //Invocar la funcion RemoveAccents, y se le pasa como parametro el texto a validar.
         string clickedButtonNameGame = EventSystem.current.currentSelectedGameObject.GetComponentInChildren<Text>().text;
         PlayerPrefs.SetString("NameGame", clickedButtonNameGame);
     }
