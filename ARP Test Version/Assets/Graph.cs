68027b4b0e7db4d8cc785f9838d83577e1abe06b
Antonio Berardi <adberardi@yahoo.com>
2023-10-24 23:41:19 -0400

    Implementando graficas

---
+++ ARP Test Version/Assets/Graph.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Graph : MonoBehaviour
+{
+    [SerializeField] private Sprite circleSprite;
+    private RectTransform graphContainer;
+
+    private void Awake()
+    {
+        graphContainer = transform.Find("graphContainer").GetComponent<RectTransform>();
+        CreateCircle(new Vector2(200, 200));
+    }
+
+    private void CreateCircle(Vector2 anchoredPosition)
+    {
+        GameObject gameObject = new GameObject("circle", typeof(Image));
+        gameObject.transform.SetParent(graphContainer, false);
+        gameObject.GetComponent<Image>().sprite = circleSprite;
+        RectTransform rectransform = gameObject.GetComponent<RectTransform>();
+        rectransform.anchoredPosition = anchoredPosition;
+        rectransform.sizeDelta = new Vector2(11, 11);
+        rectransform.anchorMin = new Vector2(0, 0);
+        rectransform.anchorMax = new Vector2(0, 0);
+    }
+
+}
